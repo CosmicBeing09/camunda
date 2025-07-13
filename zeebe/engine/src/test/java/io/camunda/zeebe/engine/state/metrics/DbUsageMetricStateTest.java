1d872b7511d491246830d6d2cb6afe2c9b74f10d
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 06:44:57 -0600

    changes to solve benchmark id 114922
     changes to solve benchmark id 114922 
    
     Renaming the identifier from createRPIMetric to recordRPIMetric likely reflects a shift in emphasis from the metric's creation to its logging or recording within the system, suggesting a more defined role in capturing usage data.

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/metrics/DbUsageMetricStateTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/metrics/DbUsageMetricStateTest.java
@@ -7,7 +7,7 @@
  */
 package io.camunda.zeebe.engine.state.metrics;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
 import io.camunda.zeebe.engine.state.mutable.MutableUsageMetricState;
@@ -23,6 +23,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(ProcessingStateExtension.class)
 public class DbUsageMetricStateTest {
+
   private MutableProcessingState processingState;
   private MutableUsageMetricState state;
 
@@ -32,12 +33,12 @@ public class DbUsageMetricStateTest {
   }
 
   @Test
-  public void shouldCreateRPIMetricProcessIncident() {
+  public void shouldRecordRPIMetricProcessIncident() {
     // given
     final var eventTime = InstantSource.system().millis();
 
     // when
-    state.createRPIMetric(eventTime, 123L, TenantOwned.DEFAULT_TENANT_IDENTIFIER);
+    state.recordRPIMetric(eventTime, 123L, TenantOwned.DEFAULT_TENANT_IDENTIFIER);
 
     // then
     final var actual = state.getTenantIdPIsMapByEventTime(eventTime);
@@ -52,10 +53,10 @@ public class DbUsageMetricStateTest {
     final var eventTime1 = InstantSource.system().millis();
     final var eventTime2 =
         InstantSource.offset(InstantSource.system(), Duration.ofSeconds(10)).millis();
-    state.createRPIMetric(eventTime1, 123L, TenantOwned.DEFAULT_TENANT_IDENTIFIER);
-    state.createRPIMetric(eventTime2, 10L, "tenant1");
-    state.createRPIMetric(eventTime2, 11L, "tenant1");
-    state.createRPIMetric(eventTime2, 12L, "tenant2");
+    state.recordRPIMetric(eventTime1, 123L, TenantOwned.DEFAULT_TENANT_IDENTIFIER);
+    state.recordRPIMetric(eventTime2, 10L, "tenant1");
+    state.recordRPIMetric(eventTime2, 11L, "tenant1");
+    state.recordRPIMetric(eventTime2, 12L, "tenant2");
 
     // when
     final var actual1 = state.getTenantIdPIsMapByEventTime(eventTime1);
@@ -76,10 +77,10 @@ public class DbUsageMetricStateTest {
     final var eventTime1 = InstantSource.system().millis();
     final var eventTime2 =
         InstantSource.offset(InstantSource.system(), Duration.ofSeconds(10)).millis();
-    state.createRPIMetric(eventTime1, 123L, TenantOwned.DEFAULT_TENANT_IDENTIFIER);
-    state.createRPIMetric(eventTime2, 10L, "tenant1");
-    state.createRPIMetric(eventTime2, 11L, "tenant1");
-    state.createRPIMetric(eventTime2, 12L, "tenant2");
+    state.recordRPIMetric(eventTime1, 123L, TenantOwned.DEFAULT_TENANT_IDENTIFIER);
+    state.recordRPIMetric(eventTime2, 10L, "tenant1");
+    state.recordRPIMetric(eventTime2, 11L, "tenant1");
+    state.recordRPIMetric(eventTime2, 12L, "tenant2");
     assertThat(state.getTenantIdPIsMapByEventTime(eventTime1))
         .containsExactlyInAnyOrderEntriesOf(
             Map.of(TenantOwned.DEFAULT_TENANT_IDENTIFIER, List.of(123L)));
