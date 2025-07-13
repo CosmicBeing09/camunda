82a19334fde3cc7a5843b1a9d2e4df31af8108df
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 13:54:37 -0600

    seed rename for 118241

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/RoleAddEntityProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/RoleAddEntityProcessor.java
@@ -135,11 +135,11 @@ public class RoleAddEntityProcessor implements DistributedTypedRecordProcessor<R
   }
 
   private boolean isEntityPresent(
-      final String entityId, final EntityType entityType, final boolean internalGroupsEnabled) {
+      final String entityId, final EntityType entityType, final boolean groupsClaimEnabled) {
     return switch (entityType) {
       case USER, CLIENT -> true; // With simple mappings, any username and client id can be assigned
       case MAPPING -> mappingState.get(entityId).isPresent();
-      case GROUP -> !internalGroupsEnabled || groupState.get(entityId).isPresent();
+      case GROUP -> !groupsClaimEnabled || groupState.get(entityId).isPresent();
       default -> false;
     };
   }
