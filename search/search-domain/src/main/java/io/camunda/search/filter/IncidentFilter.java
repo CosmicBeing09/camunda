83c4c747e27a0cf39516d43e76aa6b6e85499bbc
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 04:00:45 -0600

    changes to solve benchmark id 104061
     The rename from treePaths to treePath suggests a shift from representing multiple paths to a single path, which can clarify the intended usage and simplify the code's structure.

--- search/search-domain/src/main/java/io/camunda/search/filter/IncidentFilter.java
+++ search/search-domain/src/main/java/io/camunda/search/filter/IncidentFilter.java
@@ -29,7 +29,7 @@ public record IncidentFilter(
     List<Long> flowNodeInstanceKeys,
     DateValueFilter creationTime,
     List<IncidentState> states,
-    List<String> treePaths,
+    List<String> treePath,
     List<Long> jobKeys,
     List<String> tenantIds)
     implements FilterBase {
@@ -47,7 +47,7 @@ public record IncidentFilter(
     private List<Long> flowNodeInstanceKeys;
     private DateValueFilter creationTimeFilter;
     private List<IncidentState> states;
-    private List<String> treePaths;
+    private List<String> treePath;
     private List<Long> jobKeys;
     private List<String> tenantIds;
 
@@ -146,12 +146,12 @@ public record IncidentFilter(
       return this;
     }
 
-    public Builder treePaths(final String value, final String... values) {
+    public Builder treePath(final String value, final String... values) {
       return treePaths(collectValues(value, values));
     }
 
     public Builder treePaths(final List<String> values) {
-      treePaths = addValuesToList(treePaths, values);
+      treePath = addValuesToList(treePath, values);
       return this;
     }
 
@@ -187,7 +187,7 @@ public record IncidentFilter(
           Objects.requireNonNullElse(flowNodeInstanceKeys, Collections.emptyList()),
           creationTimeFilter,
           Objects.requireNonNullElse(states, Collections.emptyList()),
-          Objects.requireNonNullElse(treePaths, Collections.emptyList()),
+          Objects.requireNonNullElse(treePath, Collections.emptyList()),
           Objects.requireNonNullElse(jobKeys, Collections.emptyList()),
           Objects.requireNonNullElse(tenantIds, Collections.emptyList()));
     }
