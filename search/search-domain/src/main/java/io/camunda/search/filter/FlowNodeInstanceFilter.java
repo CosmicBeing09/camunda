c324e56bb320173ed9e922c648094b0773f94412
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 13:55:07 -0600

    seed rename for 120871

--- search/search-domain/src/main/java/io/camunda/search/filter/FlowNodeInstanceFilter.java
+++ search/search-domain/src/main/java/io/camunda/search/filter/FlowNodeInstanceFilter.java
@@ -19,22 +19,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
-public record FlowNodeInstanceFilter(
-    List<Long> flowNodeInstanceKeys,
-    List<Long> processInstanceKeys,
-    List<Long> processDefinitionKeys,
-    List<String> processDefinitionIds,
-    List<Operation<String>> stateOperations,
-    List<FlowNodeType> types,
-    List<String> flowNodeIds,
-    List<String> flowNodeNames,
-    List<String> treePaths,
-    Boolean hasIncident,
-    List<Long> incidentKeys,
-    List<String> tenantIds,
-    List<String> startDates,
-    List<String> endDates)
-    implements FilterBase {
+FilterBase {
 
   public static FlowNodeInstanceFilter of(
       final Function<FlowNodeInstanceFilter.Builder, ObjectBuilder<FlowNodeInstanceFilter>> fn) {
@@ -169,13 +154,13 @@ public record FlowNodeInstanceFilter(
       return tenantIds(collectValuesAsList(values));
     }
 
-    public FlowNodeInstanceFilter.Builder startDates(final List<String> values) {
+    public FlowNodeInstanceFilter.Builder startDateOperations(final List<String> values) {
       startDates = addValuesToList(startDates, values);
       return this;
     }
 
     public FlowNodeInstanceFilter.Builder startDates(final String... values) {
-      return startDates(collectValuesAsList(values));
+      return startDateOperations(collectValuesAsList(values));
     }
 
     public FlowNodeInstanceFilter.Builder endDates(final List<String> values) {
@@ -207,3 +192,19 @@ public record FlowNodeInstanceFilter(
     }
   }
 }
+    implements
+public record FlowNodeInstanceFilter(
+    List<Long> flowNodeInstanceKeys,
+    List<Long> processInstanceKeys,
+    List<Long> processDefinitionKeys,
+    List<String> processDefinitionIds,
+    List<Operation<String>> stateOperations,
+    List<FlowNodeType> types,
+    List<String> flowNodeIds,
+    List<String> flowNodeNames,
+    List<String> treePaths,
+    Boolean hasIncident,
+    List<Long> incidentKeys,
+    List<String> tenantIds,
+    List<String> startDates,
+    List<String> endDates)
