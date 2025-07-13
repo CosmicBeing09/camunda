1328137f71124d4e8a60203d3583205ce7acc338
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 13:41:56 -0600

    seed rename for 103861

--- search/search-domain/src/main/java/io/camunda/search/filter/BatchOperationFilter.java
+++ search/search-domain/src/main/java/io/camunda/search/filter/BatchOperationFilter.java
@@ -59,10 +59,10 @@ public record BatchOperationFilter(
     }
 
     public Builder state(final String value, final String... values) {
-      return state(collectValues(value, values));
+      return stateOperations(collectValues(value, values));
     }
 
-    public Builder state(final List<String> values) {
+    public Builder stateOperations(final List<String> values) {
       state = addValuesToList(state, values);
       return this;
     }
