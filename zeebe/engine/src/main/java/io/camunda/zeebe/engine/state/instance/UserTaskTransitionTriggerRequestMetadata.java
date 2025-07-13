9f6b7e37d4c8953b4f2de0d1ebf3c0738685c1e6
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 06:36:00 -0600

    changes to solve benchmark id 112928
     changes to solve benchmark id 112928 
    
     The rename from getTriggerType to getValueType likely reflects a shift towards a more generalized and consistent naming convention that better aligns with the overarching role of the method in retrieving the type of value being processed within the request.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/instance/UserTaskTransitionTriggerRequestMetadata.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/instance/UserTaskTransitionTriggerRequestMetadata.java
@@ -66,7 +66,7 @@ public class UserTaskTransitionTriggerRequestMetadata extends UnpackedObject imp
    *
    * @return The trigger type (for instance: `USER_TASK` or `VARIABLE_DOCUMENT`).
    */
-  public ValueType getTriggerType() {
+  public ValueType getValueType() {
     return triggerTypeProperty.getValue();
   }
 
@@ -76,7 +76,7 @@ public class UserTaskTransitionTriggerRequestMetadata extends UnpackedObject imp
    * @param triggerType The trigger type (`USER_TASK` or `VARIABLE_DOCUMENT`).
    * @return this metadata instance.
    */
-  public UserTaskTransitionTriggerRequestMetadata setTriggerType(final ValueType triggerType) {
+  public UserTaskTransitionTriggerRequestMetadata setValueType(final ValueType triggerType) {
     triggerTypeProperty.setValue(triggerType);
     return this;
   }
