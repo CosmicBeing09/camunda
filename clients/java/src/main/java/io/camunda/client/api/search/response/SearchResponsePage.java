686152f616bba31d938b5bb199cce2515a2fab8e
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-14 01:05:53 -0600

    changes to solve benchmark id 116371
     changes to solve benchmark id 116371 
    
     The rename from searchBeforeCursor to startCursor likely reflects a clearer and more intuitive naming convention that aligns with standard pagination terminology, enhancing the readability and consistency of the code.

--- clients/java/src/main/java/io/camunda/client/api/search/response/SearchResponsePage.java
+++ clients/java/src/main/java/io/camunda/client/api/search/response/SearchResponsePage.java
@@ -21,7 +21,7 @@ public interface SearchResponsePage {
   Long totalItems();
 
   /** The sort values of the first item in the returned page. */
-  String searchBeforeCursor();
+  String startCursor();
 
   /** The sort values of the last item in the returned page. */
   String searchAfterCursor();
