bfeb92ed07f85a1ff7c660cc1b04d486ac6a98c0
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 22:42:22 -0600

    changes to solve benchmark id 115129
     The rename from flowNodeInstanceKeys to flowNodeInstanceKeyOperations clarifies that the list now represents a collection of operations associated with flow node instance keys, rather than the keys themselves.

--- search/search-client-query-transformer/src/main/java/io/camunda/search/clients/transformers/filter/IncidentFilterTransformer.java
+++ search/search-client-query-transformer/src/main/java/io/camunda/search/clients/transformers/filter/IncidentFilterTransformer.java
@@ -55,7 +55,7 @@ public class IncidentFilterTransformer extends IndexFilterTransformer<IncidentFi
     final var errorTypeQuery = getErrorTypeQuery(filter.errorTypes());
     final var errorMessageQuery = getErrorMessageQuery(filter.errorMessages());
     final var flowNodeIdQuery = getFlowNodeIdQuery(filter.flowNodeIds());
-    final var flowNodeInstanceKeyQuery = getFlowNodeInstanceKeyQuery(filter.flowNodeInstanceKeys());
+    final var flowNodeInstanceKeyQuery = getFlowNodeInstanceKeyQuery(filter.flowNodeInstanceKeyOperations());
     final var creationTimeQuery = getCreationTimeQuery(filter.creationTime());
     final var stateQuery = getStateQuery(filter.states());
     final var treePathQuery = getTreePathQuery(filter.treePath());
