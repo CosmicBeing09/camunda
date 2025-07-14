b5faa18cd6976c6b6a3b97f7e77b681a3a291ddf
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-14 00:00:47 -0600

    commit changes for analysis

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/api/ProcessingResultBuilder.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/api/ProcessingResultBuilder.java
@@ -28,9 +28,9 @@ public interface ProcessingResultBuilder {
    *     RecordBatch
    */
   default ProcessingResultBuilder appendRecord(
-      final long key, final RecordValue value, final RecordMetadata metadata)
+      final long key, final RecordValue value, final RecordMetadata request)
       throws RuntimeException {
-    final var either = appendRecordReturnEither(key, value, metadata);
+    final var either = appendRecordReturnEither(key, value, request);
 
     if (either.isLeft()) {
       // This is how we handled too big record batches as well, except that this is now a
@@ -51,7 +51,7 @@ public interface ProcessingResultBuilder {
    * @return returns either a failure or itself for chaining
    */
   Either<RuntimeException, ProcessingResultBuilder> appendRecordReturnEither(
-      final long key, final RecordValue value, final RecordMetadata metadata);
+      final long key, final RecordValue value, final RecordMetadata request);
 
   /**
    * Sets the response for the result; will be overwritten if called more than once
