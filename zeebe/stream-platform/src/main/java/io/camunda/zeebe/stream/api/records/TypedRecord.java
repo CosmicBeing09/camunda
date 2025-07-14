2f6e7141f447e395e1b0c51d6b504e0f0c519d7d
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 22:13:54 -0600

    The rename from `metadata` to `request` clarifies the role of the identifier in encapsulating specific request details tied to asynchronous operations, making the code more intuitive by directly conveying that it pertains to an ongoing request rather than general metadata.

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/api/records/TypedRecord.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/api/records/TypedRecord.java
@@ -28,7 +28,7 @@ public interface TypedRecord<T extends UnifiedRecordValue> extends Record<T> {
 
   int getLength();
 
-  default boolean hasRequestMetadata() {
+  default boolean hasRequest() {
     return getRequestId() != RecordMetadataEncoder.requestIdNullValue()
         && getRequestStreamId() != RecordMetadataEncoder.requestStreamIdNullValue();
   }
