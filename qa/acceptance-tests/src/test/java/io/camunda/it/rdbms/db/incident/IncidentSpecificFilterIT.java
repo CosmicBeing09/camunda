816fcd1eb49c90bea8208d250d5515725145c124
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 22:38:17 -0600

    The rename from errorMessages to errorMessageOperations indicates a shift towards a more operation-focused naming convention, emphasizing that the identifier now represents a list of operations that can be performed on error messages rather than simply a collection of error messages themselves.

--- qa/acceptance-tests/src/test/java/io/camunda/it/rdbms/db/incident/IncidentSpecificFilterIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/rdbms/db/incident/IncidentSpecificFilterIT.java
@@ -97,7 +97,7 @@ public class IncidentSpecificFilterIT {
         new IncidentFilter.Builder().flowNodeIds("sorting-flow-node").build(),
         new IncidentFilter.Builder().flowNodeInstanceKeys(4000L).build(),
         new IncidentFilter.Builder().errorTypes(ErrorType.JOB_NO_RETRIES).build(),
-        new IncidentFilter.Builder().errorMessages("error-message-5000").build(),
+        new IncidentFilter.Builder().errorMessageOperations("error-message-5000").build(),
         new IncidentFilter.Builder().states(IncidentState.ACTIVE).build(),
         new IncidentFilter.Builder().jobKeys(6000L).build(),
         new IncidentFilter.Builder()
