1075dbb8e385c5f745f7854b631cb30bb4534135
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-13 22:16:56 -0600

    changes to solve benchmark id 112927
     The identifier was renamed to remove the reference to "request metadata," suggesting that this aspect is no longer relevant to the test's primary focus on user task cancellation in the absence of intermediate state.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/usertask/UserTaskProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/usertask/UserTaskProcessor.java
@@ -260,7 +260,7 @@ public class UserTaskProcessor implements TypedRecordProcessor<UserTaskRecord> {
 
     persistedRecord.setDeniedReason(command.getValue().getDeniedReason());
     final var recordRequestMetadata =
-        userTaskState.findRecordRequestMetadata(persistedRecord.getUserTaskKey());
+        userTaskState.findRecordMetadata(persistedRecord.getUserTaskKey());
 
     stateWriter.appendFollowUpEvent(persistedRecord.getUserTaskKey(), intent, persistedRecord);
     recordRequestMetadata.ifPresent(
