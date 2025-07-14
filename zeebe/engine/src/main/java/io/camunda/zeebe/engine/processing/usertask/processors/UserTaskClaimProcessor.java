24b150cc7537d9b1f0afe3e171b0e2eb1c06484e
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-14 00:10:44 -0600

    commit changes for analysis

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/usertask/processors/UserTaskClaimProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/usertask/processors/UserTaskClaimProcessor.java
@@ -87,10 +87,10 @@ public final class UserTaskClaimProcessor implements UserTaskCommandProcessor {
       responseWriter.writeEventOnCommand(
           userTaskKey, UserTaskIntent.ASSIGNED, userTaskRecord, command);
     } else {
-      final var recordRequestMetadata = userTaskState.findRecordMetadata(userTaskKey);
+      final var recordMetadata = userTaskState.findRecordMetadata(userTaskKey);
       stateWriter.appendFollowUpEvent(userTaskKey, UserTaskIntent.ASSIGNED, userTaskRecord);
 
-      recordRequestMetadata.ifPresent(
+      recordMetadata.ifPresent(
           metadata ->
               responseWriter.writeResponse(
                   userTaskKey,
