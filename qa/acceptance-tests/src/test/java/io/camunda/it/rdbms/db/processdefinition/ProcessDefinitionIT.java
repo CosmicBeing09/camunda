61d3847114d74b47a5787249eea484a1a6583441
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-18 02:48:03 -0600

    commit changes for analysis

--- qa/acceptance-tests/src/test/java/io/camunda/it/rdbms/db/processdefinition/ProcessDefinitionIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/rdbms/db/processdefinition/ProcessDefinitionIT.java
@@ -36,10 +36,10 @@ public class ProcessDefinitionIT {
   @TestTemplate
   public void shouldSaveAndFindProcessDefinitionByKey(
       final CamundaRdbmsTestApplication testApplication) {
-    final RdbmsService rdbmsService = testApplication.getRdbmsService();
-    final RdbmsWriter rdbmsWriter = rdbmsService.createWriter(PARTITION_ID);
+    final RdbmsService service = testApplication.getRdbmsService();
+    final RdbmsWriter rdbmsWriter = service.createWriter(PARTITION_ID);
     final ProcessDefinitionReader processDefinitionReader =
-        rdbmsService.getProcessDefinitionReader();
+        service.getProcessDefinitionReader();
 
     final var processDefinition = ProcessDefinitionFixtures.createRandomized(b -> b);
     createAndSaveProcessDefinition(rdbmsWriter, processDefinition);
@@ -57,10 +57,10 @@ public class ProcessDefinitionIT {
   @TestTemplate
   public void shouldFindProcessInstanceByBpmnProcessId(
       final CamundaRdbmsTestApplication testApplication) {
-    final RdbmsService rdbmsService = testApplication.getRdbmsService();
-    final RdbmsWriter rdbmsWriter = rdbmsService.createWriter(PARTITION_ID);
+    final RdbmsService service = testApplication.getRdbmsService();
+    final RdbmsWriter rdbmsWriter = service.createWriter(PARTITION_ID);
     final ProcessDefinitionReader processDefinitionReader =
-        rdbmsService.getProcessDefinitionReader();
+        service.getProcessDefinitionReader();
 
     final var processDefinition =
         ProcessDefinitionFixtures.createRandomized(
