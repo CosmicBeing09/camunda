cc01fd72fe72c66862951837c2ce9721dd60c9b3
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-18 00:13:43 -0600

    commit changes for analysis

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/instance/DbUserTaskState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/instance/DbUserTaskState.java
@@ -26,8 +26,8 @@ public class DbUserTaskState implements MutableUserTaskState {
   // key => user task record value
   // we need two separate wrapper to not interfere with get and put
   // see https://github.com/zeebe-io/zeebe/issues/1914
-  private final UserTaskRecordValue userTaskRecordToRead = new UserTaskRecordValue();
-  private final UserTaskRecordValue userTaskRecordToWrite = new UserTaskRecordValue();
+  private final UserTaskRecordValue readableUserTaskRecord = new UserTaskRecordValue();
+  private final UserTaskRecordValue writableUserTaskRecord = new UserTaskRecordValue();
 
   private final DbLong userTaskKey;
 
@@ -67,7 +67,7 @@ public class DbUserTaskState implements MutableUserTaskState {
 
     userTasksColumnFamily =
         zeebeDb.createColumnFamily(
-            ZbColumnFamilies.USER_TASKS, transactionContext, userTaskKey, userTaskRecordToRead);
+            ZbColumnFamilies.USER_TASKS, transactionContext, userTaskKey, readableUserTaskRecord);
 
     statesUserTaskColumnFamily =
         zeebeDb.createColumnFamily(
@@ -99,8 +99,8 @@ public class DbUserTaskState implements MutableUserTaskState {
   public void createUserTask(final UserTaskRecord userTask) {
     userTaskKey.wrapLong(userTask.getUserTaskKey());
     // do not persist variables in user task state
-    userTaskRecordToWrite.setRecordWithoutVariables(userTask);
-    userTasksColumnFamily.insert(userTaskKey, userTaskRecordToWrite);
+    writableUserTaskRecord.setRecordWithoutVariables(userTask);
+    userTasksColumnFamily.insert(userTaskKey, writableUserTaskRecord);
     // initialize state
     userTaskState.setLifecycleState(LifecycleState.CREATING);
     statesUserTaskColumnFamily.insert(fkUserTask, userTaskState);
@@ -110,8 +110,8 @@ public class DbUserTaskState implements MutableUserTaskState {
   public void updateUserTask(final UserTaskRecord userTask) {
     userTaskKey.wrapLong(userTask.getUserTaskKey());
     // do not persist variables in user task state
-    userTaskRecordToWrite.setRecordWithoutVariables(userTask);
-    userTasksColumnFamily.update(userTaskKey, userTaskRecordToWrite);
+    writableUserTaskRecord.setRecordWithoutVariables(userTask);
+    userTasksColumnFamily.update(userTaskKey, writableUserTaskRecord);
   }
 
   @Override
