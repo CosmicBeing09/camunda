e0f0a3854b2c7cf93caf230029eab3192c316142
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-07-18 02:00:54 -0600

    commit changes for analysis

--- clients/java/src/test/java/io/camunda/client/impl/util/EnumUtilTest.java
+++ clients/java/src/test/java/io/camunda/client/impl/util/EnumUtilTest.java
@@ -32,7 +32,7 @@ public class EnumUtilTest {
       final io.camunda.client.protocol.rest.OwnerTypeEnum protocolValue =
           EnumUtil.convert(value, io.camunda.client.protocol.rest.OwnerTypeEnum.class);
       assertThat(protocolValue).isNotNull();
-      if (value == OwnerType.UNKNOWN_ENUM_VALUE) {
+      if (value == OwnerType.UNKNOWN) {
         assertThat(protocolValue)
             .isEqualTo(io.camunda.client.protocol.rest.OwnerTypeEnum.UNKNOWN_DEFAULT_OPEN_API);
       } else {
@@ -45,7 +45,7 @@ public class EnumUtilTest {
       final OwnerType value = EnumUtil.convert(protocolValue, OwnerType.class);
       assertThat(value).isNotNull();
       if (protocolValue == io.camunda.client.protocol.rest.OwnerTypeEnum.UNKNOWN_DEFAULT_OPEN_API) {
-        assertThat(value).isEqualTo(OwnerType.UNKNOWN_ENUM_VALUE);
+        assertThat(value).isEqualTo(OwnerType.UNKNOWN);
       } else {
         assertThat(value.name()).isEqualTo(protocolValue.name());
       }
